7d7907204e88aea5cf9d95cd7f310d43dce83394
hxchua00 <hxchua00@gmail.com>
2021-09-29 15:25:49 +0800

    Add project files.

---
+++ NoDuplicateOperands/Program.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NoDuplicateOperands
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int x =4, y=4;
+            try
+            {
+                RandomMethod(x, y);
+            }
+            catch (NoDuplicateOperandsException e)
+            {
+                Console.WriteLine(e.Message);
+            }
+
+            Console.ReadLine();
+        }
+
+        public static void RandomMethod(int a, int b)
+        {
+            
+            try
+            {
+                int add = a + b;
+                
+            }
+            catch(NoDuplicateOperandsException e)
+            {
+                if(a == b)
+                {
+                    throw e;
+                }
+            }
+            
+        }
+
+        class NoDuplicateOperandsException : Exception
+        {
+            public NoDuplicateOperandsException() { }
+
+            public NoDuplicateOperandsException(string msg) 
+                :base(String.Format("No Duplicate Operands Allowed! {0}", msg))
+            {
+                msg = "No Duplicate Operands Allowed!";
+            }
+        }
+    }
+}
